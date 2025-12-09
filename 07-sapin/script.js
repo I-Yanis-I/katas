4cbf6bf843195e4c0124b1ec58e31b59ad538eef
Yanis <yanis.blanchard@gmail.com>
2025-12-09 15:58:51 +0100

    Reorganize exercises with consistent naming (dash-case)

---
+++ 07-sapin/script.js
@@ -0,0 +1,30 @@
+// function afficherPointeSapin(hauteur) {
+//   // à remplir
+// }
+
+// afficherPointeSapin(4)
+
+
+
+// function afficherEtoiles(n) {
+//     let star = "";
+//     for (let i = 0; i < n; i++) {
+//         star = star + "*"
+//     }
+//     console.log(star)
+// };
+
+// afficherEtoiles(1);
+// afficherEtoiles(5);
+
+
+function afficherRectangle(hauteur, largeur) {
+
+  for (let i = 0; i < n; i++) {
+      largeur = largeur + "*";
+      hauteur = "\n";
+  }
+  console.log(largeur);
+}
+
+afficherRectangle(5, 5);
---
+++ 07-sapin/tempCodeRunnerFile.js
@@ -0,0 +1,11 @@
+
+// function afficherEtoiles(n) {
+//     let star = "";
+//     for (let i = 0; i < n; i++) {
+//         star = star + "*"
+//     }
+//     console.log(star)
+// };
+
+// afficherEtoiles(1);
+// afficherEtoiles(5);
